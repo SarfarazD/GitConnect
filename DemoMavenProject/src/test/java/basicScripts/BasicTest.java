9b53cf020203a8d0eef65b2989aa9a54192b02f6
sarfarazdoni <donisarfaraz@gmail.com>
2016-09-28 14:34:57 +0530

    2.34

--- DemoMavenProject/src/test/java/basicScripts/BasicTest.java
+++ DemoMavenProject/src/test/java/basicScripts/BasicTest.java
@@ -72,6 +72,6 @@ public class BasicTest extends ReportMain {
 		driver.get("http://google.com");
 		driver.findElement(By.id("gs_htif0")).sendKeys("Software");
 		System.out.println("Done 123");
-		System.out.println("12111121212132342423");
+		System.out.println("1211112121213234242a3");
 	}
 }
