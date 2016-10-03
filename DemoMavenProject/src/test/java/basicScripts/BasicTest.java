5354176bc048e404efd0a5511a1ffd5993c751a7
sarfarazdoni <donisarfaraz@gmail.com>
2016-10-03 18:53:15 +0530

    Final Test

--- DemoMavenProject/src/test/java/basicScripts/BasicTest.java
+++ DemoMavenProject/src/test/java/basicScripts/BasicTest.java
@@ -1,82 +1,52 @@
 package basicScripts;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
-
 import factory.Report.ReportMain;
-import factory.Report.Utility;
-import utility.Browser;
-import utility.JavaHelpers;
+
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;	
 
 
 public class BasicTest extends ReportMain {
 	
-	//WebDriver driver;
-	/*
-	private WebDriver driver;
-	private Browser b = new Browser();
-	JavaHelpers JH = new JavaHelpers();
-	private ExtentTest test;
 	
-	
-	@Parameters({ "browser", "environment", "os" })
-	@BeforeMethod
-	public void setUp(String browser, String environment , String OS) throws Exception 
-	{		
-		driver= b.setUp(browser, environment, OS);
-		
-	}    
-
-	@AfterMethod
-	public void tearDown(ITestResult result) 
-	{
-	if(result.getStatus()==ITestResult.FAILURE)
-	{
-	 
-	String screenshot_path=Utility.captureScreenshot(driver, result.getName());
-	String image= test.addScreenCapture(screenshot_path);
-	test.log(LogStatus.FAIL, "Title verification", image);
-	 
-	 
-	}
-	 
-	extent.endTest(test);
-	extent.flush();
-	 
-	driver.get("D:\\Report\\CTtransit_Test_Execution_Report.html");
-	driver.close();
-	}
-
-	/* Test 1: 
-	 * Verify that user can login with valid credentials
-	*/ 
 	@Test
 	private void Test() throws Exception
 	{
-		//test = extent.startTest("Verify User is able to search the trip!!");	
 		
-		//SeleniumFunctions SeleniumFunc = new SeleniumFunctions(driver);
-	
-		//SeleniumFunc.ToGoToUrl(Constants.ApplicationURL);
-		//System.setProperty("webdriver.chrome.driver", "src/test/resources/BrowserDrivers/chromedriver.exe");
 		 WebDriver driver = new HtmlUnitDriver();
-		//WebDriver driver = new ChromeDriver();
+		 driver.get("https://qa1.boat-ed.com/");
 		
+		 driver.findElement(By.cssSelector(".dropdown-toggle")).click();
+		 
+		 List<WebElement> list=driver.findElements(By.cssSelector("#state-listing"));
+		 
+		 
+		 
+		    System.out.println("No of links present="+ list.size());
+		    
+		  
+		   for(WebElement wb: list){
+		        System.out.println(wb.getText());
+		        
+		   // for(int i=0;i<list.size();i++){
+		        //System.out.println(list.get(i).getAttribute("href"));
+		    }
+		    System.out.println("end of test");
+		    
+		    driver.quit();
+		 
+		
+		/*
 		driver.manage().window().maximize();
 		driver.get("http://clarionwebsiteqa.com/");
 
 		System.out.println("-----Automatic Buid Execution is Done when user commit and push the code in GitHub  Server-----");
-		driver.close();
+		driver.close();*/
 	}
 }
