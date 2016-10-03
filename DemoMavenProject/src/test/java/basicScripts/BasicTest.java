b36878a9062c50a7feb0b84ee5eb9570f5e36b3e
sarfarazdoni <donisarfaraz@gmail.com>
2016-10-03 16:07:40 +0530

    ff

--- DemoMavenProject/src/test/java/basicScripts/BasicTest.java
+++ DemoMavenProject/src/test/java/basicScripts/BasicTest.java
@@ -75,7 +75,7 @@ public class BasicTest extends ReportMain {
 		driver.manage().window().maximize();
 		driver.get("http://clarionwebsiteqa.com/");
 
-		System.out.println("-----Automatic Buid Execution is Done when user commit and push the code in GitHub  sfsdfsd------");
+		System.out.println("-----Automatic Buid Execution is Done when user commit and push the code in GitHub  sfsdfsd-ff-----");
 		driver.close();
 	}
 }
