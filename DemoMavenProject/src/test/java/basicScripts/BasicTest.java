9723e52faf2f3db3d2d1714167ad458e4a955024
sarfarazdoni <donisarfaraz@gmail.com>
2016-10-04 11:51:48 +0530

    Test Demo

--- DemoMavenProject/src/test/java/basicScripts/BasicTest.java
+++ DemoMavenProject/src/test/java/basicScripts/BasicTest.java
@@ -1,10 +1,6 @@
 package basicScripts;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import factory.Report.ReportMain;
@@ -18,35 +14,15 @@ public class BasicTest extends ReportMain {
 	@Test
 	private void Test() throws Exception
 	{
-		
-		 WebDriver driver = new HtmlUnitDriver();
-		 driver.get("https://qa1.boat-ed.com/");
-		
-		 driver.findElement(By.cssSelector(".dropdown-toggle")).click();
-		 
-		 List<WebElement> list=driver.findElements(By.cssSelector("#state-listing"));
-		 
-		 
-		 
-		    System.out.println("No of links present="+ list.size());
-		    
-		  
-		   for(WebElement wb: list){
-		        System.out.println(wb.getText());
-		        
-		   // for(int i=0;i<list.size();i++){
-		        //System.out.println(list.get(i).getAttribute("href"));
-		    }
-		    System.out.println("end of test");
-		    
-		    driver.quit();
-		 
-		
 		/*
-		driver.manage().window().maximize();
-		driver.get("http://clarionwebsiteqa.com/");
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/BrowserDrivers/chromedriver.exe");
+		   driver = new ChromeDriver();*/
+		
+		WebDriver driver = new HtmlUnitDriver();
+		 driver.get("http://clarionwebsiteqa.com/");
 
-		System.out.println("-----Automatic Buid Execution is Done when user commit and push the code in GitHub  Server-----");
-		driver.close();*/
+		System.out.println("-----Automation Jenkins CI-----");
+		driver.close();
 	}
 }
